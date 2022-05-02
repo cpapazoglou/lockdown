9be1edef90a98bed97d5e566505ad5e48fb96308
cpap <papazoglou.charalampos@gmail.com>
2022-05-02 15:29:34 +0300

    Updates

--- src/Constants.js
+++
@@ -1,3 +0,0 @@
-export const Actions = [ { id: 1, title: "Φαρμακείο / Γιατρός"},{ id: 2, title: "Είδη Πρώτης Ανάγκης"},{ id: 3, title: "Τράπεζα / Δημόσια Υπηρεσία"},{ id: 4, title: "Παροχή Βοήθειας / Συνοδεία Μαθητών"},{ id: 5, title: "Διαζευγμένοι Γονείς / Κηδεία"},{ id: 6, title: "Άσκηση / Κατοικίδιο"}];
-export const smsNumber = '13033';
-export const lockdownExpiryDate = new Date('12/1/2020');
